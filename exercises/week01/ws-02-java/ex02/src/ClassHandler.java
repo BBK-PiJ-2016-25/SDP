a300a3b64809aa146e4a634ce53d44d2ed4b46a6
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-02-05 22:23:42 +0000

    Getting git up to date- test

--- exercises/week01/ws-02-java/ex02/src/ClassHandler.java
+++ exercises/week01/ws-02-java/ex02/src/ClassHandler.java
@@ -1,9 +1,11 @@
+import java.lang.reflect.Constructor;
+
 /**
  * Created by anniehawes on 05/02/2017.
  */
 public class ClassHandler {
 
     public void construct(Class classToBeConstructed, String[] arguments) {
-        arguments.length()
+        Constructor[] constructors = classToBeConstructed.getConstructors();
     }
 }
--- exercises/week01/ws-02-java/ex02/src/Launcher.java
+++ exercises/week01/ws-02-java/ex02/src/Launcher.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 /**
  * Created by anniehawes on 05/02/2017.
  */
 public class Launcher {
     public static void main(String[] args) {
-        ClassHandler classHandler = new ClassHandler();
-        classHandler.construct(Class.forName(args[0]), args[1:]);
+        // ClassHandler classHandler = new ClassHandler();
+        // classHandler.construct(Class.forName(args[0]), args[1:]);
+
+        System.out.println(Arrays.toString(Class.forName(java.util.Date).getConstructors()));
+
     }
 }
