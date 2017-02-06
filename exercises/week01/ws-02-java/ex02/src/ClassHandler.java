6e7dd4e9613c8bcbca7d725416c337ffc6b856ea
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-02-06 23:42:24 +0000

    Completed exercises 2, worksheet 2

--- exercises/week01/ws-02-java/ex02/src/ClassHandler.java
+++ exercises/week01/ws-02-java/ex02/src/ClassHandler.java
@@ -1,11 +1,63 @@
 import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by anniehawes on 05/02/2017.
  */
 public class ClassHandler {
 
-    public void construct(Class classToBeConstructed, String[] arguments) {
+    /**
+     * Constructs a new instance of a class, with the desired specification passed as arguments.
+     * Arguments must be specified in the correct order as shown in the constructor
+     * @param classToBeConstructed the class to be constructed, as a String
+     * @param arguments arguments for the class, provided in an array of Strings alternating
+     * between type and associated value
+     * @return the requested class instance.
+     */
+    public Object construct(Class classToBeConstructed, String[] arguments) throws Exception {
+        // Splitting the arguments array into types and parameters
+        ArrayList types = new ArrayList();
+        ArrayList parameters = new ArrayList();
+        // Using the type flag to apportion Strings to types or parameters appropriately
+        boolean type = true;
+        int j = 0;
+        for (int i = 0; i < arguments.length; i++) {
+            if (type) {
+                types.add(arguments[i]);
+            } else {
+                parameters.add(arguments[i]);
+                j++;
+            }
+            type = !type;
+        }
+        // Fetching the class's constructors
         Constructor[] constructors = classToBeConstructed.getConstructors();
+        Constructor constructor = constructors[0];
+        // Checking which of the constructors matches the given parameter types
+        for(Constructor candidate : constructors) {
+            if (Arrays.toString(candidate.getParameterTypes()).equals(Arrays.toString(types.toArray()))) {
+                constructor = candidate;
+            }
+        }
+        // Converting the parameters array into varargs readable format
+        Object[] parametersArgs = parameters.toArray();
+        for(int counter = 0; counter < parametersArgs.length; counter++) {
+            System.out.println(constructor.getParameterTypes()[counter].toString());
+            if(constructor.getParameterTypes()[counter].toString().equals("long")) {
+                parametersArgs[counter] = Long.parseLong(parametersArgs[counter].toString());
+            } else if (constructor.getParameterTypes()[counter].toString().equals("int")) {
+                parametersArgs[counter] = Integer.parseInt(parametersArgs[counter].toString());
+            } else if (constructor.getParameterTypes()[counter].toString().equals("float")){
+                parametersArgs[counter] = Float.parseFloat(parametersArgs[counter].toString());
+            } else {
+                parametersArgs[counter] = constructor.getParameterTypes()[counter].cast(parametersArgs[counter]);
+                counter++;
+            }
+        }
+
+        System.out.println(constructor.toString());
+        System.out.println(Arrays.toString(parametersArgs));
+        return constructor.newInstance(parametersArgs);
     }
 }
--- exercises/week01/ws-02-java/ex02/src/Launcher.java
+++ exercises/week01/ws-02-java/ex02/src/Launcher.java
@@ -1,14 +1,13 @@
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * Created by anniehawes on 05/02/2017.
  */
 public class Launcher {
-    public static void main(String[] args) {
-        // ClassHandler classHandler = new ClassHandler();
-        // classHandler.construct(Class.forName(args[0]), args[1:]);
-
-        System.out.println(Arrays.toString(Class.forName(java.util.Date).getConstructors()));
-
+    public static void main(String[] args) throws Exception {
+        ClassHandler classHandler = new ClassHandler();
+        Date date = (Date) classHandler.construct(Class.forName(args[0]), Arrays.copyOfRange(args, 1, args.length));
+        System.out.println(date.toString());
     }
 }
