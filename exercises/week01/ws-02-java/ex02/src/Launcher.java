50e902b1201ba60ba84a98b778a8e276ba9a982c
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-02-06 23:43:16 +0000

    Removed run configuration specific code from launcher

--- exercises/week01/ws-02-java/ex02/src/Launcher.java
+++ exercises/week01/ws-02-java/ex02/src/Launcher.java
@@ -7,7 +7,6 @@ import java.util.Date;
 public class Launcher {
     public static void main(String[] args) throws Exception {
         ClassHandler classHandler = new ClassHandler();
-        Date date = (Date) classHandler.construct(Class.forName(args[0]), Arrays.copyOfRange(args, 1, args.length));
-        System.out.println(date.toString());
+        classHandler.construct(Class.forName(args[0]), Arrays.copyOfRange(args, 1, args.length));
     }
 }
