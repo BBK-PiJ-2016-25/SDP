8667c95857668525ce0a57cb6319d0337515aab4
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-02-07 00:32:31 +0000

    Added initial implementation of first three tests ex 3

---
+++ exercises/week01/ws-02-java/ex03/src/SubmittedProgramShould.java
@@ -0,0 +1,59 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by anniehawes on 07/02/2017.
+ */
+public class SubmittedProgramShould {
+
+    Class studentsClass = Class.forName("TestClass");
+
+    @Test
+    public void submittedProgramShouldHaveFourOrLessFields {
+        assertTrue(studentsClass.getDeclaredFields().length <= 4);
+    }
+
+    @Test
+    public void submittedProgramShouldHaveOnlyPrivateFields {
+        Object[] nonPrivateFields = Arrays.stream(studentsClass.getDeclaredFields())
+                .filter(s -> !s.toString(s.getModifiers()).equals("private")
+                .toArray();
+        assertTrue(nonPrivateFields.length = 0);
+    }
+
+    @Test
+    public void submittedProgramShouldHaveNoFieldsOfTypeArrayList {
+        Object[] fieldsOfTypeArrayList = Arrays.stream(studentsClass.getDeclaredFields())
+                .filter(s -> s.getType() == Class.forName(java.util.ArrayList))
+                        .toArray();
+        assertTrue(fieldsOfTypeArrayList.length = 0);
+    }
+
+    @Test
+    public void submittedProgramShouldHaveLessThanTwoPrivateHelperMethods {
+
+    }
+
+    @Test
+    public void submittedProgramShouldHaveNoMethodsWithAThrowClause {
+
+    }
+
+    @Test
+    public void submittedProgramShouldHaveNoMethodsThatReturnAnInt {
+
+    }
+
+    @Test
+    public void submittedProgramShouldHaveAZeroArgumentConstructor {
+
+    }
+
+
+
+}
