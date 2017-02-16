cb3b773a08527e80c789f0bc67a34c68ab53a601
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-02-16 12:38:40 +0000

    Configured alarm system dependency injection with xml

--- exercises/week02/ws-SOLID/alarmsystem-scala/src/main/ControlUnit.scala
+++ exercises/week02/ws-SOLID/alarmsystem-scala/src/main/ControlUnit.scala
@@ -1,18 +1,15 @@
 package main
 
-// import org.springframework.context.support.ClassPathXmlApplicationContext
+import org.springframework.context.support.ClassPathXmlApplicationContext
 
 import scala.collection.mutable.ListBuffer
 
 class ControlUnit {
   def pollSensors() {
-    //val factory = new ClassPathXmlApplicationContext("dixmlcons.xml")
-
-    // val sensors = new ListBuffer[Sensor]()
+    val factory = new ClassPathXmlApplicationContext("dixmlcons.xml")
     val sensors = new ListBuffer[Sensor]
-    sensors += new FireSensor()
-    // sensors += factory.getBean("firstSensor").asInstanceOf[Sensor]
-    sensors += new SmokeSensor()
+    sensors += factory.getBean("firstSensor").asInstanceOf[Sensor]
+    sensors += factory.getBean("secondSensor").asInstanceOf[Sensor]
     for (sensor <- sensors) {
       if (sensor.isTriggered) {
         System.out.println("A " + sensor.getSensorType + " sensor was triggered at " + sensor.getLocation)
