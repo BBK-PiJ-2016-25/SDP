2192ead1e3c1c42c17825115ac7872ca476c6489
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-02-27 03:02:41 +0000

    Implemented setHead function

--- exercises/week04/src/main/scala/functions/Funcs.scala
+++ exercises/week04/src/main/scala/functions/Funcs.scala
@@ -40,7 +40,13 @@ object Funcs {
     * @param n: Int the number of elements to drop.
     * @return a list with the first n elements of ls removed, or an empty list.
     */
-  def drop[A](ls: List[A], n: Int): List[A] = ???
+  def drop[A](ls: List[A], n: Int): List[A] = {
+    (ls, n) match {
+      case (Nil, _) => Nil
+      case (h :: t, 0) => h :: t
+      case (h :: t, _) => drop(t, n-1)
+    }
+  }
 
   /**
     * init takes a list and removes the last element.
