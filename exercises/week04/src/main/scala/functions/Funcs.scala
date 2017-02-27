0e35f7fb29faa9d3dc9c476dd5785169138b9375
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-02-27 02:51:46 +0000

    Implemented setHead method

--- exercises/week04/src/main/scala/functions/Funcs.scala
+++ exercises/week04/src/main/scala/functions/Funcs.scala
@@ -26,7 +26,12 @@ object Funcs {
     * @return a list whose head is `a' and whose tail is all but the first
     * element of ls.
     */
-  def setHead[A](ls: List[A], a: A):List[A] = ???
+  def setHead[A](ls: List[A], a: A):List[A] = {
+    ls match {
+      case Nil => a :: Nil
+      case _ :: tail => a :: tail
+    }
+  }
 
   /**
     * drop removes n elements from the given list. If n is greater than the
