41690a2c4a8f627dee5e784d2edc5701d5cb135b
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-05-04 01:46:56 +0100

    Added extensive tests for all methods implemented in Film and Director

--- exercises/week05/src/test/scala/classes/FilmTest.scala
+++ exercises/week05/src/test/scala/classes/FilmTest.scala
@@ -4,31 +4,73 @@ import org.scalatest.{BeforeAndAfter, FunSuite}
 
 class FilmTest extends FunSuite with BeforeAndAfter {
 
-  test("Correct values are generated with demo data provided") {
+  var eastwood: Director = _
+  var mcTiernan: Director = _
+  var nolan: Director = _
+  var someGuy: Director = _
 
-    val eastwood = new Director("Clint", "Eastwood", 1930)
-    val mcTiernan = new Director("John", "McTiernan", 1951)
-    val nolan = new Director("Christopher", "Nolan", 1970)
-    val someGuy = new Director("Just", "Some Guy", 1990)
+  var memento: Film = _
+  var darkKnight: Film = _
+  var inception: Film = _
+  var highPlainsDrifter: Film = _
+  var outlawJoseyWales: Film = _
+  var unforgiven: Film = _
+  var granTorino: Film = _
+  var invictus: Film = _
+  var predator: Film = _
+  var dieHard: Film = _
+  var huntForRedOctober: Film = _
+  var thomasCrownAffair: Film = _
 
-    val memento = new Film("Memento", 2000, 8.5, nolan)
-    val darkKnight = new Film("Dark Knight", 2008, 9.0, nolan)
-    val inception = new Film("Inception", 2010, 8.8, nolan)
+  before {
+    eastwood = Director("Clint", "Eastwood", 1930)
+    mcTiernan = Director("John", "McTiernan", 1951)
+    nolan = Director("Christopher", "Nolan", 1970)
+    someGuy = Director("Just", "Some Guy", 1990)
 
-    val highPlainsDrifter = new Film("High Plains Drifter", 1973, 7.7, eastwood)
-    val outlawJoseyWales = new Film("The Outlaw Josey Wales", 1976, 7.9, eastwood)
-    val unforgiven = new Film("Unforgiven", 1992, 8.3, eastwood)
-    val granTorino = new Film("Gran Torino", 2008, 8.2, eastwood)
-    val invictus = new Film("Invictus", 2009, 7.4, eastwood)
+    memento = Film("Memento", 2000, 8.5, nolan)
+    darkKnight = Film("Dark Knight", 2008, 9.0, nolan)
+    inception = Film("Inception", 2010, 8.8, nolan)
 
-    val predator = new Film("Predator", 1987, 7.9, mcTiernan)
-    val dieHard = new Film("Die Hard", 1988, 8.3, mcTiernan)
-    val huntForRedOctober = new Film("The Hunt for Red October", 1990, 7.6, mcTiernan)
-    val thomasCrownAffair = new Film("The Thomas Crown Affair", 1999, 6.8, mcTiernan)
+    highPlainsDrifter = Film("High Plains Drifter", 1973, 7.7, eastwood)
+    outlawJoseyWales = Film("The Outlaw Josey Wales", 1976, 7.9, eastwood)
+    unforgiven = Film("Unforgiven", 1992, 8.3, eastwood)
+    granTorino = Film("Gran Torino", 2008, 8.2, eastwood)
+    invictus = Film("Invictus", 2009, 7.4, eastwood)
 
-    assert(eastwood.yearOfBirth == 1930)
-    assert(dieHard.director.name == "John McTiernan")
-    assert(!invictus.isDirectedBy(nolan))
+    predator = Film("Predator", 1987, 7.9, mcTiernan)
+    dieHard = Film("Die Hard", 1988, 8.3, mcTiernan)
+    huntForRedOctober = Film("The Hunt for Red October", 1990, 7.6, mcTiernan)
+    thomasCrownAffair = Film("The Thomas Crown Affair", 1999, 6.8, mcTiernan)
   }
 
+  test("yearOfBirth returns correct year of birth for director") {
+    assert(eastwood.yearOfBirth == 1930)
+    assert(mcTiernan.yearOfBirth == 1951)
+  }
+
+  test("name returns first and last names appended for director") {
+    assert(nolan.name == "Christopher Nolan")
+    assert(someGuy.name == "Just Some Guy")
+  }
+
+  test("isDirectedBy returns where correct director is supplied for a film") {
+    assert(unforgiven.isDirectedBy(eastwood))
+  }
+
+  test("directorsAge returns correct age for director at time of release") {
+    assert(predator.directorsAge == 36)
+  }
+
+  test("older returns older of two directors") {
+    assert(Director.older(nolan, eastwood).equals(eastwood))
+  }
+
+  test("highestRated returns film with higher imdb rating") {
+    assert(Film.highestRating(inception, outlawJoseyWales).equals(inception))
+  }
+
+  test("oldestDirectorAtTheTime returns older of directors at time of filming") {
+    assert(Film.oldestDirectorAtTheTime(memento, huntForRedOctober).equals(mcTiernan))
+  }
 }
