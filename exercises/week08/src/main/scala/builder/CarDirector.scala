8241fe45590922c526871060b83feb5f14c1fea9
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-05-08 01:56:33 +0100

    Implemented Builder Design Pattern

---
+++ exercises/week08/src/main/scala/builder/CarDirector.scala
@@ -0,0 +1,14 @@
+package builder
+
+case class CarDirector(carBuilder: CarBuilder) {
+  def build =  {
+    carBuilder.buildBodyStyle
+    carBuilder.buildBreaks
+    carBuilder.buildEngine
+    carBuilder.buildFuelType
+    carBuilder.buildPower
+    carBuilder.buildSeats
+    carBuilder.buildWindows
+  }
+
+}
