995466d3c8cbed2148524c8be059c42721f9d9ba
BBK-PiJ-25-2-16 <ahawes02@dcs.bbk.ac.uk>
2017-05-09 00:51:48 +0100

    Implemented Bridge Design Pattern

---
+++ exercises/week08/src/main/scala/bridge/BigWheel.scala
@@ -0,0 +1,15 @@
+package bridge
+
+case class BigWheel(product: Product, s: String) extends Car(product, s) {
+  override def assemble: Unit = {
+    println("Assembling " + product.productName + " for " + s)
+  }
+
+  override def produceProduct: Unit = {
+    product.produce
+  }
+
+  override def printDetails: Unit = {
+    println("Car: " + s + ", Product: " + product.productName)
+  }
+}
---
+++ exercises/week08/src/main/scala/bridge/GearLocking.scala
@@ -0,0 +1,9 @@
+package bridge
+
+case class GearLocking(s: String) extends Product {
+
+  override def productName: String = s
+
+  override def produce: Unit = println("Producing " + s)
+
+}
